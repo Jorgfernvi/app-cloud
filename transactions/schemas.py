90d753edf9504b419a6a057c6b43098b9c1fac9c
Jorgfernvi <38923515+Jorgfernvi@users.noreply.github.com>
2024-05-18 13:07:21 -0500

    Update schemas.py

--- transactions/schemas.py
+++ transactions/schemas.py
@@ -1,21 +1,14 @@
 from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
 
 class TransferRequest(BaseModel):
     from_account: int
     to_account: int
     amount: float
 
-class ServicePaymentRequest(BaseModel):
-    account_id: int
-    service_provider_id: int
-    amount: float
-
 class Transaction(BaseModel):
     id: int
-    from_account: Optional[int] = None
-    to_account: Optional[int] = None
+    from_account: int
+    to_account: int
     amount: float
     transaction_type: str
-    timestamp: datetime
+
